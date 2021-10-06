e51bbb3743d785b6b2cd6ec19294751c84f04cb1
Persie0 <68641445+bachletantien@users.noreply.github.com>
2021-10-06 15:49:43 +0700

    Remove redundant import

--- components/containers/layout.container.tsx
+++ components/containers/layout.container.tsx
@@ -1,6 +1,4 @@
 import Footer from 'components/containers/footer.container';
-import News from 'components/containers/news.container';
-
 export interface LayoutProps {}
 
 const Layout: React.FunctionComponent<LayoutProps> = (props) => {
