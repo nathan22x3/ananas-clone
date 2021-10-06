1340f93a129f48759c63da8dbc92dd432d0af5f7
Nathan Nguyen <67423224+nathan22x3@users.noreply.github.com>
2021-10-06 10:34:50 +0700

    Completed Footer container
    - Fixed Next Link issue when set `href` prop as '#'

--- components/containers/footer.container.tsx
+++ components/containers/footer.container.tsx
@@ -2,6 +2,10 @@ import Button from 'components/common/button.component';
 import Link from 'next/link';
 import footerContent from 'public/content/footer.content';
 import AnanasStore from 'public/svg/ananas-store.svg';
+import AnanaTextLogo from 'public/svg/ananas-text-logo.svg';
+import FacebookIcon from 'public/svg/facebook-icon.svg';
+import InstagramIcon from 'public/svg/instagram-icon.svg';
+import YoutubeIcon from 'public/svg/youtube-icon.svg';
 import tw from 'twin.macro';
 
 export interface FooterProps {}
@@ -15,7 +19,7 @@ const Footer: React.FunctionComponent<FooterProps> = (props) => {
           <span css={tw`text-2xl`}>Tìm cửa hàng</span>
         </Button>
       </section>
-      <section css={tw`col-start-2 col-span-3`}>
+      <section css={tw`col-start-2 col-span-3 flex flex-col gap-y-7`}>
         <div css={tw`grid grid-cols-4`}>
           {footerContent.map(({ title, items }) => (
             <div key={title}>
@@ -31,6 +35,49 @@ const Footer: React.FunctionComponent<FooterProps> = (props) => {
               </ul>
             </div>
           ))}
+        </div>
+        <div css={tw`grid grid-cols-4`}>
+          <div>
+            <h4 css={tw`mb-5 uppercase`}>Ananas social</h4>
+            <div css={tw`flex gap-x-1.5`}>
+              <Link href="/" passHref>
+                <a>
+                  <FacebookIcon />
+                </a>
+              </Link>
+              <Link href="/" passHref>
+                <a>
+                  <InstagramIcon />
+                </a>
+              </Link>
+              <Link href="/" passHref>
+                <a>
+                  <YoutubeIcon />
+                </a>
+              </Link>
+            </div>
+          </div>
+          <div>
+            <h4 css={tw`mb-5 uppercase`}>Đăng ký nhận mail</h4>
+            <form css={tw`flex flex-col items-start gap-2 2xl:(flex-row items-stretch)`}>
+              <input
+                type="text"
+                placeholder="Nhập email của bạn"
+                css={tw`w-48 px-4 py-2.5 outline-none text-gray-600 2xl:w-44`}
+              />
+              <Button>
+                <span>Gửi</span>
+              </Button>
+            </form>
+          </div>
+          <div css={tw`col-span-2 flex justify-start items-center`}>
+            <AnanaTextLogo />
+          </div>
+        </div>
+        <div css={tw`grid grid-cols-4`}>
+          <span css={tw`col-start-2 col-span-2 mt-8 text-gray-300`}>
+            Copyright &copy; 2021 Ananas. All rights reserved.
+          </span>
         </div>
       </section>
     </footer>
--- public/content/footer.content.ts
+++ public/content/footer.content.ts
@@ -12,34 +12,34 @@ const footerContent: FooterContent[] = [
   {
     title: 'Sản phẩm',
     items: [
-      { text: 'Giày Nam', href: '#' },
-      { text: 'Giày Nữ', href: '#' },
-      { text: 'Thời trang & Phụ kiện', href: '#' },
-      { text: 'Sale-off', href: '#' },
+      { text: 'Giày Nam', href: '/' },
+      { text: 'Giày Nữ', href: '/' },
+      { text: 'Thời trang & Phụ kiện', href: '/' },
+      { text: 'Sale-off', href: '/' },
     ],
   },
   {
     title: 'Về công ty',
     items: [
-      { text: 'Dứa tuyển dụng', href: '#' },
-      { text: 'Liên hệ nhượng quyền', href: '#' },
-      { text: 'Về Ananas', href: '#' },
+      { text: 'Dứa tuyển dụng', href: '/' },
+      { text: 'Liên hệ nhượng quyền', href: '/' },
+      { text: 'Về Ananas', href: '/' },
     ],
   },
   {
     title: 'Hỗ trợ',
     items: [
-      { text: 'FAQs', href: '#' },
-      { text: 'Bảo mật thông tin', href: '#' },
-      { text: 'Chính sách chung', href: '#' },
-      { text: 'Tra cứu đơn hàng', href: '#' },
+      { text: 'FAQs', href: '/' },
+      { text: 'Bảo mật thông tin', href: '/' },
+      { text: 'Chính sách chung', href: '/' },
+      { text: 'Tra cứu đơn hàng', href: '/' },
     ],
   },
   {
     title: 'Liên hệ',
     items: [
-      { text: 'Email góp ý', href: '#' },
-      { text: 'Hotline: 0963 429 749', href: '#' },
+      { text: 'Email góp ý', href: '/' },
+      { text: 'Hotline: 0963 429 749', href: '/' },
     ],
   },
 ];
